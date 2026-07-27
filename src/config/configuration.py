7f4b0665a6f7f2d5d778cddc6f951aae95c10bd7
Gaurav Singh <grv08singh@gmail.com>
2026-07-27 23:40:52 +0530

    configuration done

---
+++ src/config/configuration.py
@@ -0,0 +1,83 @@
+import os
+import logging
+from src.constants import *
+from src.utils.common import read_yaml, create_directories, save_json
+from src.entities.config_entity import DataIngestionConfig, DataPreProcessingConfig, DataProcessingConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig
+
+class ConfigurationManager:
+    def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
+        logging.info("Initialization started for class ConfigurationManager")
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        create_directories([self.config.artifacts_root])
+        logging.info("Initialization completed for class ConfigurationManager")
+        
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        logging.info("Entered Method: get_data_ingestion_config")
+        config = self.config.data_ingestion
+        create_directories([config.root_dir])
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            local_data_file=config.local_data_file,
+            raw_data_file=config.raw_data_file
+        )
+        logging.info("Exited Method: get_data_ingestion_config")
+        return data_ingestion_config
+        
+    def get_data_preprocessing_config(self) -> DataPreProcessingConfig:
+        logging.info("Entered Method: get_data_preprocessing_config")
+        config = self.config.data_preprocessing
+        create_directories([config.root_dir])
+        data_preprocessing_config = DataPreProcessingConfig(
+            root_dir=config.root_dir,
+            preprocessed_data=config.preprocessed_data
+        )
+        logging.info("Exited Method: get_data_preprocessing_config")
+        return data_preprocessing_config
+    
+    def get_data_processing_config(self) -> DataProcessingConfig:
+        logging.info("Entered Method: get_data_processing_config")
+        config = self.config.data_processing
+        create_directories([config.root_dir])
+        data_processing_config = DataProcessingConfig(
+            root_dir=config.root_dir,
+            processed_data=config.processed_data
+        )
+        logging.info("Exited Method: get_data_processing_config")
+        return data_processing_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        logging.info("Entered Method: get_prepare_base_model_config")
+        config = self.config.prepare_base_model
+        create_directories([config.root_dir])
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=config.root_dir,
+            base_model=config.base_model
+        )
+        logging.info("Exited Method: get_prepare_base_model_config")
+        return prepare_base_model_config
+    
+    def get_training_config(self) -> TrainingConfig:
+        logging.info("Entered Method: get_training_config")
+        config = self.config.training
+        create_directories([config.root_dir])
+        training_config = TrainingConfig(
+            root_dir=config.root_dir,
+            trained_model=config.trained_model,
+            val_report=config.val_report
+        )
+        logging.info("Exited Method: get_training_config")
+        return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        logging.info("Entered Method: get_evaluation_config")
+        config = self.config.evaluation
+        create_directories([config.root_dir])
+        evaluation_config = EvaluationConfig(
+            root_dir=config.root_dir,
+            test_dir=config.test_dir,
+            evaluation_report=config.evaluation_report,
+            mlflow_uri=config.mlflow_uri
+        )
+        logging.info("Exited Method: get_evaluation_config")
+        return evaluation_config
--- src/entities/config_entity.py
+++ src/entities/config_entity.py
@@ -4,5 +4,33 @@ from pathlib import Path
 @dataclass
 class DataIngestionConfig:
     root_dir: Path
-    src_data_file: Path
+    local_data_file: Path
     raw_data_file: Path
+
+@dataclass(frozen=True)
+class DataPreProcessingConfig:
+    root_dir: Path
+    preprocessed_data: Path
+
+@dataclass(frozen=True)
+class DataProcessingConfig:
+    root_dir: Path
+    processed_data: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model: Path
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model: Path
+    val_report: Path
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    root_dir: Path
+    test_dir: Path
+    evaluation_report: Path
+    mlflow_uri: str
