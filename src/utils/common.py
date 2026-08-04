94e4073f99bf5792ad0ed3d340ecfdd333b90109
grv08singh <grv08singh@gmail.com>
2026-08-04 17:10:56 +0530

    change structure

--- src/utils/common.py
+++ src/utils/common.py
@@ -11,10 +11,12 @@ import joblib
 
 @ensure_annotations
 def read_yaml(path: Path) -> ConfigBox:
+    logging.info("Entered Method read_yaml")
     try:
         with open(path, 'r') as yaml_file:
             content = yaml.safe_load(yaml_file)
             logging.info(f"YAML file loaded successfully: {path}")
+            logging.info("Exited Method read_yaml")
             return ConfigBox(content)
     except BoxValueError as e:
         logging.info(f"YAML file is empty: {path}")
@@ -25,31 +27,41 @@ def read_yaml(path: Path) -> ConfigBox:
 
 @ensure_annotations
 def create_directories(path_list: list, verbose=True):
+    logging.info("Entered Method create_directories")
     for path in path_list:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logging.info(f"Directory created: {path}")
+    logging.info("Exited Method create_directories")
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
+    logging.info("Entered Method save_json")
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
     logging.info(f"JSON file saved at {path}")
+    logging.info("Exited Method save_json")
 
 @ensure_annotations
 def load_json(path: Path) -> dict:
+    logging.info("Entered Method load_json")
     with open(path, 'r') as f:
         content = json.load(f)
     logging.info(f"JSON loaded: {path}")
+    logging.info("Exited Method load_json")
     return ConfigBox(content)
 
 @ensure_annotations
 def save_bin(data: Any, path: Path):
+    logging.info("Entered Method save_bin")
     joblib.dump(value=data, filename=path)
     logging.info(f"Binary file saved at {path}")
+    logging.info("Exited Method save_bin")
 
 @ensure_annotations
 def load_bin(path: Path) -> Any:
+    logging.info("Entered Method load_bin")
     data = joblib.load(filename=path)
     logging.info(f"Binary file loaded from {path}")
+    logging.info("Exited Method load_bin")
     return data
