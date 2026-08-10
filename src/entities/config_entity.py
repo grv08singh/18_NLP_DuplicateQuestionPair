17903dbcd3a545a9bdfc10f08321c484b58617e7
Gaurav Singh <grv08singh@gmail.com>
2026-08-11 01:04:29 +0530

    ent

--- src/entities/config_entity.py
+++ src/entities/config_entity.py
@@ -21,16 +21,19 @@ class DataPreProcessingConfig:
 @dataclass(frozen=True)
 class DataProcessingConfig:
     root_dir: Path
-    preprocessed_data: Path
+    model_dir: Path
     X_path: Path
-    q1_encoded: Path
-    q2_encoded: Path
-    base_emb_model: Path
+    word2idx_path: Path
     trained_emb_model: Path
+    bert_emb_model: Path
     word_pair_emb: Path
     emb_matrix: Path
-    q1_emb: Path
-    q2_emb: Path
+    emb_data_own: Path
+    q1_emb_own: Path
+    q2_emb_own: Path
+    emb_data_bert: Path
+    q1_emb_bert: Path
+    q2_emb_bert: Path
     emb_min_token_count: int
     emb_max_words: int
     emb_window_size: int
@@ -38,18 +41,26 @@ class DataProcessingConfig:
     emb_neg_samples: int
     emb_batch_size: int
     emb_epochs: int
-    
-
-@dataclass(frozen=True)
-class PrepareBaseModelConfig:
-    root_dir: Path
-    base_model: Path
+    own_emb_model: bool
 
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
-    trained_model: Path
-    val_report: Path
+    model_dir: Path
+    y_path: Path
+    emb_matrix: Path
+    emb_data_own: Path
+    q1_emb_own: Path
+    q2_emb_own: Path
+    emb_data_bert: Path
+    q1_emb_bert: Path
+    q2_emb_bert: Path
+    trained_model_with_own_emb: Path
+    trained_model_with_bert_emb: Path
+    own_val_report: Path
+    bert_val_report: Path
+    own_emb_model: bool
+    epochs: int
 
 @dataclass(frozen=True)
 class EvaluationConfig:
@@ -57,3 +68,15 @@ class EvaluationConfig:
     test_dir: Path
     evaluation_report: Path
     mlflow_uri: str
+
+@dataclass(frozen=True)
+class PredictionConfig:
+    root_dir: Path
+    chatwords_file: Path
+    word2idx_path: Path
+    emb_matrix: Path
+    own_emb_model: bool
+    bert_emb_model: Path
+    trained_model_with_own_emb: Path
+    trained_model_with_bert_emb: Path
+    seq_len: int
